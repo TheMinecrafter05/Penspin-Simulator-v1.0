fa1f75aab9c2c7f2a226c182a7a9bc61b0ff0036
TheMinecrafter05 <59798742+TheMinecrafter05@users.noreply.github.com>
2020-01-13 16:16:36 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+------------INFOS--------------
+Um das Spiel zu starten, müsst 
+ihr auf die Pen Simulator v1.0
+clicken. Falls da eine Warnung
+kommen sollte, ignoriert diese.
+liegt daran, das es eine 
+unbekannte.exe datei ist.
+
+---------WARNUNG--------------------
+Das Spiel funktioniert nur in
+neueren Browsern, die localstorage
+unterstützen. Wenn ihr es nicht wisst,
+drückt im Browser F12 und schreibt in
+der Konsole localstorage. Wenn da ein 
+Fehler kommt, geht es in diesem Browser
+nicht.
---
+++ data/index.js
@@ -0,0 +1,364 @@
+var degrees = 0;
+var spins = 100;
+var spintime = 100;
+var multiplyer = 1;
+var Actualpenspin;
+var degreenumber = 1;
+var weight = 1;
+var speed = 1;
+var speedpreis = 1.04;
+var weightpreis = 1.05;
+var multypreis = 1.055;
+var speedlimit = 2;
+var weightlimit = 100;
+var multylimit = 50;
+var pen = "./StartPen.jpg";
+let music = new Audio()
+music.src = "./Background-Music.mp3"
+music.volume = 0.3
+
+    let speedkosten = 10
+
+    let weightkosten = 15
+
+    var multykosten = 10
+
+    function save() {
+        localStorage.setItem('Speed', JSON.stringify(speed));
+        localStorage.setItem('Weigh',JSON.stringify(weight));
+        localStorage.setItem('Multy', JSON.stringify(multiplyer));
+        localStorage.setItem('Spins', JSON.stringify(spins));
+        localStorage.setItem('Pen', JSON.stringify(pen));
+      }
+      
+      function load() {
+          if(localStorage.getItem('Speed')){
+            speed = JSON.parse(localStorage.getItem('Speed'));
+          }
+
+          if(localStorage.getItem('Spins')){
+            spins = JSON.parse(localStorage.getItem('Spins'));
+          }
+        
+          if(localStorage.getItem('Weigh')){
+            weight = JSON.parse(localStorage.getItem('Weigh'));
+          }
+
+          if(localStorage.getItem('Multy')){
+            multiplyer = JSON.parse(localStorage.getItem('Multy'));
+          }
+
+          if(localStorage.getItem('Pen')){
+            pen = JSON.parse(localStorage.getItem('Pen'));
+          }
+      }
+
+      jQuery(document).ready(function(){
+        load()
+        if(pen == "./Pen2.jpg"){
+            document.getElementById("pen2").innerText = "Using Pen 2"
+    }
+          document.getElementById("penid").src=pen
+          document.getElementById("penid").style.width = "425px"
+          document.getElementById("penid").style.height = "425px"
+          document.getElementById("spintextid").innerText = "Spins: " + spins
+          document.getElementById("spinspeedid").innerText = "Speed: " + speed
+          document.getElementById("spinweightid").innerText = "Weight: " + weight
+
+          spintime -= speed+1
+          degreenumber += weight-1
+
+          if(degreenumber >= weightlimit){
+              degreenumber = 50
+              document.getElementById("weight").style.backgroundColor ="RED"
+            document.getElementById("weight").innerText = "MAX"
+          }
+
+          if(spintime <=speedlimit){
+              spintime = 2
+              document.getElementById("faster").style.backgroundColor ="RED"
+                document.getElementById("faster").innerText = "MAX"
+          }
+
+          if(multiplyer >= multylimit){
+              multiplyer = 50
+              document.getElementById("multy").style.backgroundColor ="RED"
+                document.getElementById("multy").innerText = "MAX"
+          }
+
+          clearInterval(Actualpenspin)
+
+          Actualpenspin = setInterval(function() {
+
+            music.play()
+            let rotateimage = document.getElementById("penid");
+
+            rotateimage.style.transform = `rotate(${degrees}deg)`
+
+
+            degrees+=degreenumber
+            if(degrees > 359) {
+
+                spins+=1*multiplyer
+                save()
+
+            document.getElementById("spintextid").innerText = "Spins: " + spins
+
+                degrees = 1
+            }
+        }, spintime)
+      })
+
+    Actualpenspin = setInterval(function() {
+
+        music.play()
+        let rotateimage = document.getElementById("penid");
+
+        rotateimage.style.transform = `rotate(${degrees}deg)`
+
+
+        degrees+=degreenumber
+        if(degrees > 359) {
+
+            spins+=1*multiplyer
+            save()
+
+        document.getElementById("spintextid").innerText = "Spins: " + spins
+
+            degrees = 1
+        }
+    }, spintime)
+
+    function Pen2(){
+
+        if(spins < 100000) return console.log("Zu wenig!");
+
+        if(pen == "./Pen2.jpg"){
+                document.getElementById("pen2").innerText = "Using Pen 2"
+            return;
+        }
+        pen = "./Pen2.jpg"
+        document.getElementById("penid").src=pen
+        document.getElementById("penid").style.width = "425px"
+        document.getElementById("penid").style.height = "425px"
+
+        degreenumber = 1
+
+        speed = 1
+
+        spintime = 100
+
+        multiplyer = 5
+
+        weight = 1
+
+        speedpreis += 0.02
+        weightpreis += 0.02
+        multypreis += 0.02
+
+        document.getElementById("spintextid").innerText = "Spins: " + spins
+        document.getElementById("spinspeedid").innerText = "Speed: " + speed
+        document.getElementById("spinweightid").innerText = "Weight: " + weight
+
+        if(document.getElementById("faster").innerText = "MAX"){
+            document.getElementById("faster").innerText = "Faster Spin ("+Math.floor(speedkosten)+" Spins)"
+            document.getElementById("faster").style.backgroundColor = "lightblue"
+        }
+
+        if(document.getElementById("weight").innerText = "MAX"){
+            document.getElementById("weight").innerText = "Heavier Pen ("+Math.floor(weightkosten)+" Spins)"
+            document.getElementById("weight").style.backgroundColor = "lightblue"
+        }
+
+        if(document.getElementById("multy").innerText = "MAX"){
+            document.getElementById("multy").innerText = "Spin Multiplyer ("+Math.floor(multykosten)+" Spins)"
+            document.getElementById("multy").style.backgroundColor = "lightblue"
+        }
+
+        save()
+
+        clearInterval(Actualpenspin)
+
+        Actualpenspin = setInterval(function() {
+
+            music.play()
+            let rotateimage = document.getElementById("penid");
+
+            rotateimage.style.transform = `rotate(${degrees}deg)`
+
+
+            degrees+=degreenumber
+            if(degrees > 359) {
+
+                spins+=1*multiplyer
+                save()
+
+            document.getElementById("spintextid").innerText = "Spins: " + spins
+
+                degrees = 1
+            }
+        }, spintime)
+
+    }
+
+    function Pen3(){
+       localStorage.clear()
+    }
+
+    function Pen4(){
+        clearInterval(Actualpenspin)
+    }
+
+    function Pen5(){
+        clearInterval(Actualpenspin)
+    }
+
+
+    function faster(){
+
+        if(Math.floor(spins) < Math.floor(speedkosten)) return;
+
+
+            if(spintime <=2){
+                document.getElementById("faster").style.backgroundColor ="RED"
+                document.getElementById("faster").innerText = "MAX"
+
+                spintime = 2
+                speed = 100
+                document.getElementById("spinspeedid").innerText = "Speed: " + speed
+                return;
+            }
+
+            spins -= Math.floor(speedkosten)
+
+            speedkosten *=speedpreis
+
+            document.getElementById("faster").innerText = "Faster Spin ("+Math.floor(speedkosten)+" Spins)"
+            document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+            speed++
+
+            save()
+
+            document.getElementById("spinspeedid").innerText = "Speed: " + speed
+
+            spintime -= 1
+
+        clearInterval(Actualpenspin)
+
+        Actualpenspin = setInterval(function() {
+        music.play()
+        let rotateimage = document.getElementById("penid");
+
+        rotateimage.style.transform = `rotate(${degrees}deg)`
+
+
+        degrees+=degreenumber
+        if(degrees >= 360) {
+
+            spins+=1*multiplyer
+            save()
+
+        document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+            degrees = 1
+        }
+    }, spintime)
+
+    }
+
+    function heavier(){
+
+        if(Math.floor(spins) < Math.floor(weightkosten)) return;
+
+        if(degreenumber >= 50){
+            document.getElementById("weight").style.backgroundColor ="RED"
+                document.getElementById("weight").innerText = "MAX"
+
+                degreenumber = 50
+                weight = 100
+                document.getElementById("spinweightid").innerText = "Weight: " + weight
+                return;
+            }
+
+            spins -= Math.floor(weightkosten)
+
+            weightkosten *= weightpreis
+
+            document.getElementById("weight").innerText = "Heavier Pen ("+Math.floor(weightkosten)+" Spins)"
+            document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+            weight++
+
+            save()
+
+            document.getElementById("spinweightid").innerText = "Weight: " + weight
+
+        degreenumber+=0.50
+
+        clearInterval(Actualpenspin)
+
+        Actualpenspin = setInterval(() =>{
+        music.play()
+        let rotateimage = document.getElementById("penid");
+
+        rotateimage.style.transform = `rotate(${degrees}deg)`
+
+        degrees+=degreenumber
+
+        if(degrees >= 360) {
+
+            spins+=1*multiplyer
+            save()
+
+        document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+            degrees = 1
+        }
+    }, spintime)
+    }
+
+    function multy(){
+        if(Math.floor(spins) < Math.floor(multykosten)) return;
+
+        if(multiplyer >= 50){
+            document.getElementById("multy").style.backgroundColor ="RED"
+                document.getElementById("multy").innerText = "MAX"
+
+                multiplyer = 50
+                return;
+        }
+
+            spins -= Math.floor(multykosten)
+
+            multykosten *= multypreis
+
+            document.getElementById("multy").innerText = "Spin Multiplyer ("+Math.floor(multykosten)+" Spins)"
+            document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+
+        multiplyer++
+
+        save()
+
+        clearInterval(Actualpenspin)
+
+        Actualpenspin = setInterval(() =>{
+        music.play()
+        let rotateimage = document.getElementById("penid");
+
+        rotateimage.style.transform = `rotate(${degrees}deg)`
+
+        degrees+=degreenumber
+
+        if(degrees >= 360) {
+
+            spins+=1*multiplyer
+            save()
+
+        document.getElementById("spintextid").innerText = "Spins: " + Math.floor(spins)
+
+            degrees = 1
+        }
+    }, spintime)
+    }
